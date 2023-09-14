074f75ae89a40386d6a45ef0fcd46146d3d5fa2a
Ramill <ramill_ibragimov@icloud.com>
2023-09-14 20:14:02 +0300

    Initial Commit

---
+++ ProgressBar17/ProgressBar17App.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressBar17App.swift
+//  ProgressBar17
+//
+//  Created by Ramill Ibragimov on 14.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct ProgressBar17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
